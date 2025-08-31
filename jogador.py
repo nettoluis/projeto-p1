dfe8223913dc5213c28c1e390c979bc624978723
Wedson <wedson.business@gmail.com>
2025-08-31 11:18:24 -0300

    muitasm modificações e sprite

--- jogador.py
+++ jogador.py
@@ -1,31 +1,59 @@
 import pygame
 
-class Jogador:
+class Animacao(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.sheet = pygame.image.load('PNG/Swordsman_lvl3/With_shadow/Swordsman_lvl3_attack_with_shadow.png').convert_alpha()
+        self.largura = 64
+        self.altura = 64
+        self.frame_attack = []
+        self.atual = 0
+        self.attack()
+
+        self.image = self.frame_attack[self.atual]
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (100, 100)
+
+        self.contador = 0
+        self.atacando = False
+    def attack(self):
+        for i in range(8):
+            frame = self.sheet.subsurface((i * self.largura, 0, self.largura, self.altura))
+            frame = pygame.transform.scale(frame, (self.largura * 4, self.altura * 4))
+            self.frame_attack.append(frame)
+
+    def atacar(self):
+        self.atacando = True
+        self.atual = 0
+
+    def update(self):
+        if self.atacando:
+            self.contador += 1
+            if self.contador % 5 == 0:
+                self.atual += 1
+                if self.atual >= len(self.frame_attack):
+                    self.atual = 0
+                    self.atacando = False
+                self.image = self.frame_attack[self.atual]
+
+class Jogador(Animacao):
     def __init__(self, x, y):
+        super().__init__()
         self.vida = 20
-        self.x = x
-        self.y = y  
         self.dano = 5
-        self.velocidade = 1
-        
-        self.largura = 32
-        self.altura = 32
-        self.cor = (255, 255, 255)
-        self.rect = pygame.Rect(self.x, self.y, self.largura, self.altura)
+        self.velocidade = 10
 
-    
-    def draw(self, screen):
-        self.rect.topleft = (self.x, self.y)
-        pygame.draw.rect(screen, self.cor, self.rect)
+        self.rect.topleft = (x, y)
 
-    def move(self, keys):
-        pygame.key.get_pressed()
+    def acao(self, keys):
         esquerda, direita, cima, baixo = keys[pygame.K_LEFT] or keys[pygame.K_a],  keys[pygame.K_RIGHT] or keys[pygame.K_d],  keys[pygame.K_UP] or keys[pygame.K_w], keys[pygame.K_DOWN] or keys[pygame.K_s]
         if esquerda:
-            self.x -= self.velocidade
+            self.rect.x -= self.velocidade
         if direita:
-            self.x += self.velocidade
+            self.rect.x += self.velocidade
         if cima:
-            self.y -= self.velocidade
+            self.rect.y -= self.velocidade
         if baixo:
-            self.y += self.velocidade
+            self.rect.y += self.velocidade
+        if keys[pygame.K_j]:
+            self.atacar()
--- main.py
+++ main.py
@@ -11,19 +11,23 @@ def criarJanela():
     cor_fundo = (0, 0, 0)
 
     jogador = Jogador(largura / 2, altura / 2)
+    all_sprites = pygame.sprite.Group(jogador)
 
     projeteis = []
     running = True
+    clock = pygame.time.Clock()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
         keys = pygame.key.get_pressed()
-        jogador.move(keys)
+        jogador.acao(keys)
         screen.fill(cor_fundo)
-        jogador.draw(screen)
+        all_sprites.update()
+        all_sprites.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
 
     pygame.quit()
 
